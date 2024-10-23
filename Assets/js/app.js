968695fae889268b2043a92cdeda462f7ba0b04a
unknown <you@example.com>
2024-10-23 21:43:53 +0530

    if else pending

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -9,28 +9,30 @@ btnIncrement.addEventListener('click', () => {
 
 // advance counter
 let initialValue = 0;
-
+let allValueArray = [];
 const title = document.querySelector('.initial');
 const addBtn = document.querySelector('.add-btn');
 const saveBtn = document.querySelector('.save-btn');
 const para = document.querySelector('.para');
 
-let previousvalues = [];
+
 addBtn.addEventListener('click', () => {
     initialValue++;
     title.textContent = initialValue
 }
 );
-    saveBtn.addEventListener('click', () => {
-   para.innerHTML = title.innerHTML;
-   initialValue = 0;
-   title.textContent = initialValue;
+saveBtn.addEventListener('click', () => {
+    // para.innerHTML = title.innerHTML;
+
+    title.textContent = initialValue;
+
+    allValueArray.push(initialValue)
+    para.innerText = "" + allValueArray.join(',');
+    initialValue = 0;
+    title.textContent =initialValue
 });
 
-saveBtn.addEventListener("click", () => {
-    previousvalues.push(initialValue);
-}
-)
+
 
 
 
